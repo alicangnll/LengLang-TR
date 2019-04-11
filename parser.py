1cb31df22519115fa75a25235e504eba2d477367
Ali Can Gönüllü <alicangonullu1907@gmail.com>
2019-04-11 23:56:58 +0700

    Update parser.py

--- parser.py
+++ parser.py
@@ -13,29 +13,29 @@ def parser(tokenler,i):
 	while i < len(tokenler):
 		    if tokenler[i] == 'EGER':
                     if tokenler[i + 2] == 'ESIT':
-                    if tokenler[i + 1][0:4] == "OPERATION":
-                        tokenler[i + 1] = "SAYI" + str(eval(tokenler[i + 1][5:]))
-                    if tokenler[i + 3][0:4] == "OPERATION":
-                        tokenler[i + 3] = "SAYI:" + str(eval(tokenler[i + 3][5:]))
-		    if tokenler[i + 1][0:4] == "DEGER":
-                        tokenler[i + 1] = DEGISKENAL(tokenler[i + 1][4:],semboller)
-	            if tokenler[i + 3][0:4] == "DEGER":
-                        tokenler[i + 3] = DEGISKENAL(tokenler[i + 3][4:],semboller)
-                    if tokenler[i + 1] == tokenler[i + 3]:
-                        i += 5
-                        a = i
+			if tokenler[i + 1][0:4] == "OPERATION":
+				tokenler[i + 1] = "SAYI" + str(eval(tokenler[i + 1][5:]))
+                        if tokenler[i + 3][0:4] == "OPERATION":
+				tokenler[i + 3] = "SAYI:" + str(eval(tokenler[i + 3][5:]))
+			if tokenler[i + 1][0:4] == "DEGER":
+				tokenler[i + 1] = DEGISKENAL(tokenler[i + 1][4:],semboller)
+			if tokenler[i + 3][0:4] == "DEGER":
+				tokenler[i + 3] = DEGISKENAL(tokenler[i + 3][4:],semboller)
+			if tokenler[i + 1] == tokenler[i + 3]:
+				i += 5
+				a = i
         while tokenler[a] != 'YAP':
-                            a += 1
-                 while tokenler[i] != 'YAP' and i < len(tokenler):
-                            parser(tokenler[0:a+i],i)
-                            i += 1
+		a += 1
+        while tokenler[i] != 'YAP' and i < len(tokenler):
+		parser(tokenler[0:a+i],i)
+		i += 1
 		    else:
-                 while tokenler[i] != 'YAP':
-                            i += 1
-        elif tokenler[i] == 'IKEN':
+       while tokenler[i] != 'YAP':
+	i += 1
+	elif tokenler[i] == 'IKEN':
 		_i = 0
             	    if tokenler[i - 2] == 'ESIT':
-                    if tokenler[i - 1][0:4] == "OPERATION":
+		    if tokenler[i - 1][0:4] == "OPERATION":
 			tokenler[i - 1] = "SAYI:" + str(eval(tokenler[i - 1][5:]))
                     if tokenler[i - 3][0:4] == "OPERATION":
 			tokenler[i - 3] = "SAYI:" + str(eval(tokenler[i - 3][5:]))
