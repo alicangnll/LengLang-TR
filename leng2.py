0faa96cf03a42603d493ae979c8da7f16a06dee6
Ali Can Gönüllü <alicangonullu1907@gmail.com>
2019-04-11 17:47:02 +0700

    LengLang - Python-based Turkish Experimental Language
    LengLang - Python-based Turkish Experimental Language

--- leng2.py
+++ leng2.py
@@ -16,113 +16,113 @@ def leng2(filecontents):
 			if bolge == False:
 				tk = ''
 			elif bolge == True:
-				tk = " "
+			tk = " "
 				if express != '' and isexpress == 1:
 					tks.append('OPERATION:' + express)
 					express = ''
 					tk = ''
-				elif express != '' and isexpress == 0:
+			elif express != '' and isexpress == 0:
 					tks.append('SAYI:' + express)
 					express = ''
 					tk = ''
-				elif var != '':
+			elif var != '':
 					tks.append('DEGER:' + var)
 					var = ''
 					var_basla = 0
 					tk = ''
-				elif tk == '\n' or tk == '<EOF>':
-					if express != '' and isexpress == 1:
+		elif tk == '\n' or tk == '<EOF>':
+			if express != '' and isexpress == 1:
 						tks.append('OPERATION:' + express)
 						express = ''
-					elif express != '' and isexpress == 0:
+			elif express != '' and isexpress == 0:
 						tks.append('SAYI:' + express)
 						express = ''
-					elif var != '':
+			elif var != '':
 						tks.append('DEGER:' + var)
 						var = ''
 						var_basla = 0
 						tk = ''
-					elif tk == '\t':
+			elif tk == '\t':
 						tk = ''
-					elif tk == '=' and bolge == False:
-						if express != '' and isexpress == 0:
-							tks.append('SAYI:' + express)
-							express = ''
-							if var != '':
-								tks.append('DEGER:' + var)
-								var = ''
-								var_basla = 0
-								if tks[-1] == 'ESITTIR':
-									tks[-1] = 'ESIT'
-								else:
-									tks.append('ESITTIR')
-									tk = ''
-					elif tk == '&' and bolge == False:
-										var_basla = 1
-										var += tk
-										tk = ''
-					elif var_basla == 1:
-						if tk != '+' or tk == '-' or tk == '*' or tk == '/' or tk == '%':
-												var += tk
-												tk = ''
-								else:
-						var_basla = 0
-						tk = ''
-elif tk == 'YAZDIR' or tk == 'yazdır':
-	tokenler.append('YAZDIR')
-	tk = ''
-elif tk == 'EKLE' or tk == 'ekle':
-	tokenler.append('EKLE')
-	tk = ''
-elif tk == 'CARP' or tk == 'çarp':
-	tokenler.append('CARP')
-	tk = ''
-elif tk == 'BOL' or tk == 'böl':
-	tokenler.append('BOL')
-	tk = ''
-elif tk == 'EKSI' or tk == 'eksi':
-	tokenler.append('EKSI')
-	tk = ''
-elif tk == 'BOYLEYKEN' or tk == 'böyleyken':
-	tokenler.append('BOYLEYKEN')
-	tk = ''
-elif tk == 'TEKRAR' or tk == 'tekrar':
-	tokenler.append('TEKRAR')
-	tk = ''
-elif tk == 'ÇIK' or tk == "çık":
-	tokenler.append("CIK")
-	tk = ""
-elif tk == 'yap' or tk == 'YAP':
-	tokenler.append('YAP')
-	tk = ''
-elif tk == 'eğer' or tk == 'EĞER':
-	tokenler.append('EGER')
-	tk = ''
-elif tk == 'İSE' or tk == 'ise':
-	if express != '' and isexpress == 0:
-		tokenler.append('SAYI:' + express)
-		express = ''
-		tokenler.append('ISE')
-		tk = ''
-elif tk == 'GİRDİ' or tk == 'girdi' or tk == 'GİRİŞ' or tk == 'giriş':
-	tokenler.append('GIRDI')
-	tk = ''
-	elif tk == '0' or tk == '1' or tk == '2' or tk == '3' or tk == '4' or tk == '5' or tk == '6' or tk == '7' or tk == '8' or tk == '9' or tk == '(' or tk == ')':
-		express += tk
-		tk = ''
-elif tk == '+' or tk == '-' or tk == '*' or tk == '/' or tk == '%':
-	express += tk
-	isexpress = 1
-	tk = ''
-elif tk == '"' or tk == ' "':
-	if bolge == False:
-		bolge = True
-elif bolge == True:
-	tokenler.append(string + '"')
-	string = ''
-	bolge = False
-	tk = ''
-elif bolge == True:
-	string += tk
-	tk = ''
-return tokenler
+			elif tk == '=' and bolge == False:
+				if express != '' and isexpress == 0:
+					tks.append('SAYI:' + express)
+					express = ''
+				if var != '':
+					tks.append('DEGER:' + var)
+					var = ''
+					var_basla = 0
+				if tks[-1] == 'ESITTIR':
+					tks[-1] = 'ESIT'
+				else:
+					tks.append('ESITTIR')
+				tk = ''
+			elif tk == '&' and bolge == False:
+				var_basla = 1
+				var += tk
+				tk = ''
+			elif var_basla == 1:
+				if tk != '+' or tk == '-' or tk == '*' or tk == '/' or tk == '%':
+					var += tk
+					tk = ''
+				else:
+					var_basla = 0
+					tk = ''
+			elif tk == 'YAZDIR' or tk == 'yazdır':
+				tokenler.append('YAZDIR')
+				tk = ''
+			elif tk == 'EKLE' or tk == 'ekle':
+				tokenler.append('EKLE')
+				tk = ''
+			elif tk == 'CARP' or tk == 'çarp':
+				tokenler.append('CARP')
+				tk = ''
+			elif tk == 'BOL' or tk == 'böl':
+				tokenler.append('BOL')
+				tk = ''
+			elif tk == 'EKSI' or tk == 'eksi':
+				tokenler.append('EKSI')
+				tk = ''
+			elif tk == 'BOYLEYKEN' or tk == 'böyleyken':
+				tokenler.append('BOYLEYKEN')
+				tk = ''
+			elif tk == 'TEKRAR' or tk == 'tekrar':
+				tokenler.append('TEKRAR')
+				tk = ''
+			elif tk == 'ÇIK' or tk == "çık":
+				tokenler.append("CIK")
+				tk = ""
+			elif tk == 'yap' or tk == 'YAP':
+				tokenler.append('YAP')
+				tk = ''
+			elif tk == 'eğer' or tk == 'EĞER':
+				tokenler.append('EGER')
+				tk = ''
+			elif tk == 'İSE' or tk == 'ise':
+				if express != '' and isexpress == 0:
+					tokenler.append('SAYI:' + express)
+					express = ''
+				tokenler.append('ISE')
+					tk = ''
+			elif tk == 'GİRDİ' or tk == 'girdi' or tk == 'GİRİŞ' or tk == 'giriş':
+				tokenler.append('GIRDI')
+				tk = ''
+			elif tk == '0' or tk == '1' or tk == '2' or tk == '3' or tk == '4' or tk == '5' or tk == '6' or tk == '7' or tk == '8' or tk == '9' or tk == '(' or tk == ')':
+				express += tk
+				tk = ''
+			elif tk == '+' or tk == '-' or tk == '*' or tk == '/' or tk == '%':
+				express += tk
+				isexpress = 1
+				tk = ''
+			elif tk == '"' or tk == ' "':
+				if bolge == False:
+					bolge = True
+				elif bolge == True:
+					tokenler.append(string + '"')
+					string = ''
+					bolge = False
+					tk = ''
+			elif bolge == True:
+				string += tk
+				tk = ''
+	return tokenler
